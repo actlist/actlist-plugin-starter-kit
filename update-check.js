996ae603ec5b3be179047740b3bff725704755e0
silentsoft <hyesung.lee@silentsoft.org>
2019-04-01 23:03:51 +0900

    add sample `jar` response value to update-check.js

--- update-check.js
+++ update-check.js
@@ -3,6 +3,7 @@
 //             the alphabetical character calculation like 'alpha' or 'beta' is excluded on this script.
 //====================================================================================================================
 var latestVersion = '1.3.0';		// change this value to what you want.
+var jar = 'https://github.com/your-github-account/actlist-plugin-starter-kit/releases/download/v1.3.0/starter-kit-1.3.0.jar';
 var url = 'https://github.com/your-github-account/actlist-plugin-starter-kit/releases'; // change this value to what you want.
 //var killSwitchUntil   = '1.1.0';	// fill this value if necessary.
 //var endOfServiceUntil = '1.2.0';	// fill this value if necessary.
@@ -13,6 +14,7 @@ var url = 'https://github.com/your-github-account/actlist-plugin-starter-kit/rel
 (function() {
 	return {
 		'available': available(),
+		'jar': jar,
 		'url': url,
 		'endOfService': endOfService(),
 		'killSwitch': killSwitch()
