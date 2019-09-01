83f897b05b4c605124704ea1aa4ed377c72aae12
silentsoft <hyesung.lee@silentsoft.org>
2019-09-02 02:24:19 +0900

    upgrade actlist-plugin dependency to 1.6.0

--- update-check.js
+++ update-check.js
@@ -4,7 +4,7 @@
 //====================================================================================================================
 var latestVersion = '1.3.0';		// change this value to what you want.
 var jar = 'https://github.com/your-github-account/actlist-plugin-starter-kit/releases/download/v1.3.0/starter-kit-1.3.0.jar';
-//var requiredActlist   = '1.5.1';	// fill this value if necessary.
+//var requiredActlist   = '1.6.0';	// fill this value if necessary.
 var url = 'https://github.com/your-github-account/actlist-plugin-starter-kit/releases'; // change this value to what you want.
 //var killSwitchUntil   = '1.1.0';	// fill this value if necessary.
 //var endOfServiceUntil = '1.2.0';	// fill this value if necessary.
